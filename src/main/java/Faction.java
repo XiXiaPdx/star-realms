9a54f298364fa6a660fb28a4b19530b3c864b124
Shane Stafford and Xi Xia <xi@>
2017-03-29 12:04:43 -0700

    update with faction class

--- src/main/java/Faction.java
+++ src/main/java/Faction.java
@@ -1,4 +1,5 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Faction {
   private String f_name;
@@ -16,4 +17,40 @@ public class Faction {
     return f_id;
   }
 
+  @Override
+  public boolean equals(Object otherFaction) {
+    if (!(otherFaction instanceof Faction)) {
+      return false;
+    } else {
+      Faction newFaction = (Faction) otherFaction;
+      return this.getFactionName().equals(newFaction.getFactionName()) && this.getFactionId() == newFaction.getFactionId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO factions (f_name) VALUES (:f_name);";
+      this.f_id = (int) con.createQuery(sql, true)
+        .addParameter("f_name", this.f_name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Faction> all() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM factions;";
+      return con.createQuery(sql).executeAndFetch(Faction.class);
+    }
+  }
+
+  public static Faction find(int id) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM factions WHERE f_id = :f_id;";
+      Faction foundFaction = con.createQuery(sql)
+        .addParameter("f_id", id)
+        .executeAndFetchFirst(Faction.class);
+        return foundFaction;
+      }
+    }
 }
--- src/test/java/FactionTest.java
+++ src/test/java/FactionTest.java
@@ -7,4 +7,41 @@ public class FactionTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+  public void createsNewFaction_True() {
+    Faction newFaction = new Faction("Mercs");
+    assertTrue(newFaction instanceof Faction);
+  }
+
+  @Test
+  public void returnsFactionName_Mercs() {
+    Faction newFaction = new Faction("Mercs");
+    assertEquals("Mercs", newFaction.getFactionName());
+  }
+
+  @Test
+  public void getsAllFactions_True() {
+    Faction newFaction1 = new Faction("Mercs");
+    Faction newFaction2 = new Faction("Blob");
+    newFaction1.save();
+    newFaction2.save();
+    assertTrue(newFaction2.equals(Faction.all().get(1)));
+  }
+
+  @Test
+  public void findsFactionByID() {
+    Faction newFaction = new Faction("Mercs");
+    newFaction.save();
+    assertTrue(newFaction.equals(Faction.find(newFaction.getFactionId())));
+  }
+
+  @Test
+  public void getFactionId_true(){
+    Faction newFaction = new Faction("Mercs");
+    newFaction.save();
+    assertTrue(newFaction.getFactionId()>0);
+  }
+
+
+
 }
